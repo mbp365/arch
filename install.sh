a6150760e55c4a3b3cdc4cdc5b7da6209fbebb14
Max <maxkovalovs@gmail.com>
2017-10-27 18:01:50 +0100

    Updated package list

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
-sudo mv pacman.conf /etc/pacman.conf
+#sudo cp pacman.conf /etc/pacman.conf
+sudo pacman -Syu
 sudo pacman -S $(<pacman_dependencies.txt) --needed --noconfirm
-yaourt -S $(<yaourt_dependencies.txt) --needed --noconfirm
-mv -v .config/i3/config ~/.config/i3/config
-mv -v .scripts/ ~/
+yaourt -S $(<yaourt_dependencies.txt) --m-arg --skippgpcheck --needed --noconfirm
+cp -rv .config ~/
+cp -rv .scripts ~/
 systemctl enable slim.service
-sudo mv -v slim-minimal/ /usr/share/slim/themes/
-sudo mv -v slim.conf /etc/slim.conf
-sudo mv -v clipboard /etc/lib/urxvt/perl/
+sudo cp -rv slim-minimal/ /usr/share/slim/themes/
+sudo cp -rv slim.conf /etc/slim.conf
+sudo mkdir /etc/lib/urxvt/perl
+sudo cp -rv clipboard /etc/lib/urxvt/perl/
+cp -rv .Xdefaults ~/
+cp -rv .vimrc ~/
