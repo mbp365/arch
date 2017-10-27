68679c7c181498c0c1fc696e3d188eeb1956fbb1
Max <maxkovalovs@gmail.com>
2017-10-27 18:35:48 +0100

    Fix urxvt copy paste
    And some minor stuff in i3 config

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#sudo cp pacman.conf /etc/pacman.conf
+sudo cp pacman.conf /etc/pacman.conf
 sudo pacman -Syu
 sudo pacman -S $(<pacman_dependencies.txt) --needed --noconfirm
 yaourt -S $(<yaourt_dependencies.txt) --m-arg --skippgpcheck --needed --noconfirm
@@ -7,7 +7,7 @@ cp -rv .scripts ~/
 systemctl enable slim.service
 sudo cp -rv slim-minimal/ /usr/share/slim/themes/
 sudo cp -rv slim.conf /etc/slim.conf
-sudo mkdir /etc/lib/urxvt/perl
-sudo cp -rv clipboard /etc/lib/urxvt/perl/
+sudo cp -rv clipboard /usr/lib/urxvt/perl/
 cp -rv .Xdefaults ~/
 cp -rv .vimrc ~/
+sudo cp -rv xorg.conf /etc/X11/xorg.conf
