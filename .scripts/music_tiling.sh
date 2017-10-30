96ba06a02edd0f8e5676ae96022288b1f8444698
Max <maxkovalovs@gmail.com>
2017-10-30 21:24:29 +0000

    changed my music setup a bit

--- .scripts/music_tiling.sh
+++ .scripts/music_tiling.sh
@@ -1,6 +1,7 @@
 transparency=60
 urxvt -tr -sh $transparency -e ncmpcpp &
 sleep 0.1
-urxvt -tr -sh $transparency -e vis &
+urxvt -fn "xft:monospace:size=3" -tr -sh $transparency -e vis &
+sleep 0.1
 i3-msg "gaps inner current set 80;gaps outer current set 30"
 i3-msg "move down"
