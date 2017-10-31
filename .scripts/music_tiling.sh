f8faf2e46764f39165207349f6e6f3dc53c0b082
Max <maxkovalovs@gmail.com>
2017-10-31 22:19:39 +0000

    switched to a different visualizer

--- .scripts/music_tiling.sh
+++ .scripts/music_tiling.sh
@@ -1,7 +1,7 @@
-transparency=60
+transparency=40
 urxvt -tr -sh $transparency -e ncmpcpp &
 sleep 0.1
-urxvt -fn "xft:monospace:size=3" -tr -sh $transparency -e vis &
+urxvt -fn "xft:monospace:size=5" -tr -sh 100 -e cava &
 sleep 0.1
 i3-msg "gaps inner current set 80;gaps outer current set 30"
-i3-msg "move down"
+i3-msg "move up"
