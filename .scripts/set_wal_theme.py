44899951fb3178758fb60c1459a436a1887c760f
Max <maxkovalovs@gmail.com>
2017-11-05 14:33:07 +0000

    fixed padding issues

--- .scripts/set_wal_theme.py
+++ .scripts/set_wal_theme.py
@@ -2,20 +2,32 @@ import json
 import os
 import sys
 
-os.system("wal -i "+sys.argv[1])
+os.system("wal -i " + sys.argv[1])
 
 with open('/home/max/.cache/wal/colors.json') as data_file:
     data = json.load(data_file)
 
 colorscheme = {"icons": ["awesome-fonts"],
-                "defaults":{
-                    "padding":"",
-                    "separator":"◀",
-                    "separator-block-width": 0,
-                    "warning":{"fg": data["special"]["foreground"], "bg": data["colors"]["color2"]},
-                    "critical":{"fg": data["special"]["foreground"], "bg": data["colors"]["color3"]},
-                    "bg":data["special"]["background"],
-                    "fg":data["colors"]["color1"]}}
+               "defaults": {
+                   "padding": " ",
+                   "separator": "◀",
+                   "separator-block-width": 0,
+                   "warning": {"fg": data["special"]["foreground"], "bg": data["colors"]["color2"]},
+                   "critical": {"fg": data["special"]["foreground"], "bg": data["colors"]["color3"]},
+                   "bg": data["special"]["background"],
+                   "fg": data["colors"]["color1"]},
+               "mpd": {"padding": "",
+                       "playing": {"prefix": " "},
+                       "paused": {"prefix": " "},
+                       "stopped": {"prefix": ""},
+                       "prev": {"prefix": ""},
+                       "next": {"prefix": ""},
+                       "shuffle-on": {"prefix": ""},
+                       "shuffle-off": {"prefix": ""},
+                       "repeat-on": {"prefix": "  "},
+                       "repeat-off": {"prefix": "  "}
+                       }
+               }
 
 with open('/usr/share/bumblebee-status/themes/wal.json', 'w') as outfile:
     json.dump(colorscheme, outfile, sort_keys=True, indent=4,
