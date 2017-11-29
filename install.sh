7cae7a5fde20d0794d91437b5716c4520c7ed2e5
Max <maxkovalovs@gmail.com>
2017-11-29 19:23:01 +0000

    changed install.sh aur package manager to pacaur

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
-sudo cp pacman.conf /etc/pacman.conf
+#!/bin/sh
+
 sudo pacman -Syu
 sudo pacman -S $(<pacman_dependencies.txt) --needed --noconfirm
-yaourt -S $(<yaourt_dependencies.txt) --m-arg --skippgpcheck --needed --noconfirm
+#install pacaur
+curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
+pacaur -S $(<aur_dependencies.txt) --m-arg --skippgpcheck --needed --noconfirm
 cp -rv .config ~/
 cp -rv .scripts ~/
-systemctl enable slim.service
-sudo cp -rv slim-minimal/ /usr/share/slim/themes/
-sudo cp -rv slim.conf /etc/slim.conf
-sudo cp -rv clipboard /usr/lib/urxvt/perl/
-cp -rv .Xdefaults ~/
-cp -rv .vimrc ~/
-sudo cp -rv xorg.conf /etc/X11/xorg.conf
+sudo cp -v slim.conf /etc/slim.conf
+sudo cp -v clipboard /usr/lib/urxvt/perl/
+sudo cp -v override.conf /etc/systemd/system/getty@tty1.service.d/override.conf
+cp -v .Xdefaults ~/
+cp -v .bashrc ~/
+cp -v .vimrc ~/
+sudo cp -v xorg.conf /etc/X11/xorg.conf
