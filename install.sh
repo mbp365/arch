6773fbe7f493000fc06897580ef300ff14932c61
Max Kovalovs <maxkovalovs@gmail.com>
2018-01-01 04:02:40 +0000

    Removed slim, added dependencies, and more.
    Switched from nightly to normal firefox, added urxvt perls.

--- install.sh
+++ install.sh
@@ -3,12 +3,10 @@
 sudo pacman -Syu
 sudo pacman -S $(<pacman_dependencies.txt) --needed --noconfirm
 #install pacaur
-curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
+sudo curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
 pacaur -S $(<aur_dependencies.txt) --m-arg --skippgpcheck --needed --noconfirm
 cp -rv .config ~/
 cp -rv .scripts ~/
-sudo cp -v slim.conf /etc/slim.conf
-sudo cp -v clipboard /usr/lib/urxvt/perl/
 sudo cp -v override.conf /etc/systemd/system/getty@tty1.service.d/override.conf
 cp -v .Xdefaults ~/
 cp -v .bashrc ~/
--- slim-minimal/README.md
+++
@@ -1,3 +0,0 @@
-Custom [SLIM](https://wiki.archlinux.org/index.php/SLiM) theme!
-
-Panel and slim.conf taken from http://www.techtimejourney.net/category/slim-display-manager-themes/
