e4dc1da2da71c36921a70e131df7580cbc1ed3cb
Max Kovalovs <maxkovalovs@gmail.com>
2018-03-03 15:14:05 +0000

    Added git shortcuts to ranger.

--- .scripts/screencap.sh
+++ .scripts/screencap.sh
@@ -4,16 +4,16 @@
 
 #Picks a file name for the output file based on availability:
 
-if [[ -f ~/output.mkv ]]
+if [[ -f ~/output.mp4 ]]
 	then
 		n=1
-		while [[ -f $HOME/output_$n.mkv ]]
+		while [[ -f $HOME/output_$n.mp4 ]]
 		do	
 			n=$((n+1))
 		done
-		filename="$HOME/output_$n.mkv"
+		filename="$HOME/output_$n.mp4"
 	else
-		filename="$HOME/output.mkv"
+		filename="$HOME/output.mp4"
 fi
 
 #The actual ffmpeg command:
@@ -23,4 +23,4 @@ ffmpeg -y \
 -s $(xdpyinfo | grep dimensions | awk '{print $2;}') \
 -i :0.0 \
  -f pulse -ac 2 -i default \
- -c:v libx264 -r 30 -c:a flac $filename
+ -c:v libx264 -r 30 $filename
