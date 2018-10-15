e5ecb3bc626f746624122fb02dc5781b23771773
Max Kovalovs <maxkovalovs@gmail.com>
2018-10-15 23:15:37 +0100

    Fixed cava - config file now owned by user not root

--- .scripts/generate_theme.sh
+++ .scripts/generate_theme.sh
@@ -1,7 +1,9 @@
 python3 -m pywal -i $1
+USER="$(whoami)"
 #resizes the picture to the screen resolution
 #and moves it to the slim theme folder (hardcoded)
 sudo -s source ~/.cache/wal/colors.sh
 sudo python3 ~/.scripts/set_wal_theme.py $1
+sudo chown $USER ~/.config/cava/config
 sleep 2
 i3-msg restart
